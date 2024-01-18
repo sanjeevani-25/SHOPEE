82706a9c06bcc9ca451d3d6dec2f0bc87aeddaa5
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-01-18 19:25:45 +0530

    db connect

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require("./app");
-
+const connectDatabase = require("./config/database");
 const dotenv = require("dotenv");
 
 // Setting up config file
 dotenv.config({ path: "backend/config/config.env" });
+
+// Connecting to DB
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
