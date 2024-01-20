40208f1da35ee2238e57dfb4842824c23da53c0e
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-01-20 11:03:34 +0530

    get by id

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,56 @@
-exports.getProducts = (req, res, next) => {
+const Product = require("../models/product");
+
+// Crete new product => api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
   res.status(200).json({
     success: true,
-    messsage: "This route will show all products in DB.",
+    count: products.length,
+    products,
+  });
+};
+
+//  Get single product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (product == null) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Update product => /api/v1/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (product == null) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: True,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.send(200).json({
+    success: true,
+    product,
   });
 };
