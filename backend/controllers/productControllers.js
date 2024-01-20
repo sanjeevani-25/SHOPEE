efd77544e99de445622a04a510893708f8619f18
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-01-20 12:47:44 +0530

    error middlewraes

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const ErrorHandler = require("../utils/errorHandler");
 
 // Crete new product => api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
@@ -23,10 +24,11 @@ exports.getProducts = async (req, res, next) => {
 exports.getSingleProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (product == null) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    // return res.status(404).json({
+    //   success: false,
+    //   message: "Product not found",
+    // });
+    return next(new ErrorHandler("Product not found", 404));
   }
   res.status(200).json({
     success: true,
