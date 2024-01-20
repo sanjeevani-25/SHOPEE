88a9d044d584679695fbf8cd3f6949997de61b87
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-01-20 12:55:45 +0530

    separate dev & prod error handling message

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,9 +2,19 @@ const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal server error.";
-  res.status(err.statusCode).json({
-    success: false,
-    error: err.stack,
-  });
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message || "Internal server error.",
+    });
+  }
 };
