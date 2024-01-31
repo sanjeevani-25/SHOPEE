16d4bec34a5d17b1b582e0be169ab3df9082dac5
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-01 00:07:49 +0530

    filter

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -14,7 +14,7 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter();
   // const products = await Product.find();
   const products = await apiFeatures.query;
   res.status(200).json({
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -15,6 +15,20 @@ class APIFeatures {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    // removing fields from query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((e) => delete queryCopy[e]);
+
+    // advance filter for prices,ratings,etc
+    let queryStr = JSON.stringify(queryCopy);
+    // console.log(queryStr);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    // console.log(queryStr);
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
