b4c4d631190792bd5ef3639547bb5faa6a8bb98c
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-01 00:26:40 +0530

    pagination

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -14,12 +14,19 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter();
+  const resPerPage = 4;
+  const productCount = await Product.countDocuments();
+
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resPerPage);
   // const products = await Product.find();
   const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     count: products.length,
+    productCount,
     products,
   });
 });
