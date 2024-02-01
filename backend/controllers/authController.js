1c67be8614d8d08f1ca6739d955d519bfdaa174f
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-02 00:49:19 +0530

    user auth

---
+++ backend/controllers/authController.js
@@ -0,0 +1,22 @@
+const User = require("../models/user");
+
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+
+// register a user /api/v1/register
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "avatars/jeez",
+      url: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  });
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
