ac6ac10868a30c3db432eaf048eef52c1288985d
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-02 19:41:05 +0530

    sned jwt token in cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // register a user /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -15,11 +16,12 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       url: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   });
-  const token = user.getJwtToken();
-  res.status(201).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
+  // const token = user.getJwtToken();
+  // res.status(201).json({
+  //   success: true,
+  //   token,
+  // });
 });
 
 // login user /api/v1/login
@@ -40,9 +42,10 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   if (!isPasswordMatched) {
     return next(new ErrorHandler("Invalid password", 401));
   }
-  const token = user.getJwtToken();
-  res.status(201).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 201, res);
+  // const token = user.getJwtToken();
+  // res.status(201).json({
+  //   success: true,
+  //   token,
+  // });
 });
