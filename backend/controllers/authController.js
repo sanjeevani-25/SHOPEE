51eecc68174f6aba556db65e014202062021372f
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-02 14:36:58 +0530

    jwt token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,8 +15,9 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       url: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   });
+  const token = user.getJwtToken();
   res.status(201).json({
     success: true,
-    user,
+    token,
   });
 });
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -49,5 +50,12 @@ userSchema.pre("save", async function (next) {
   }
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+// return jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
