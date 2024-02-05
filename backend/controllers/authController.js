241945a5bab925b31e7ef3ef170c5c10fe6eabcd
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-05 23:14:33 +0530

    logout and delete cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -42,10 +42,22 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   if (!isPasswordMatched) {
     return next(new ErrorHandler("Invalid password", 401));
   }
-  sendToken(user, 201, res);
+  sendToken(user, 200, res);
   // const token = user.getJwtToken();
   // res.status(201).json({
   //   success: true,
   //   token,
   // });
+});
+
+// logout user => /api/v1/logout
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
 });
