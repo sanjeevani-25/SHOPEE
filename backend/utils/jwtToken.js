09b8bfece188abce11223621c5d12c5d219400de
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2024-02-22 16:07:18 +0530

    recovery email and reset password

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,7 +4,7 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
   // console.log(token);
 
-  //   options fro cookie
+  //   options for cookie
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
